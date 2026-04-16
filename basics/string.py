49ad7ea869c8f3ba95c7c876dc100214c2417760
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-16 09:40:42 +0530

    Create string and its type

---
+++ basics/string.py
@@ -0,0 +1,8 @@
+# This code demonstrates the creation and usage of a string in python.
+Full_name = "Anujith Raj"
+# Printing the string, its type, and accessing individular characters using their index, where the index starts from 0.
+print(Full_name)
+print(type(Full_name))
+print(Full_name[0])  # Accessing the first character
+print(Full_name[1])  # Accessing the second character
+print(Full_name[6])  # Accessing the seventh character
