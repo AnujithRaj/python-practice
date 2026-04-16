a9e814544e6ee282879e27bf9754d5e1a4b9fa8b
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-16 07:04:00 +0530

    Add swap_variable.py with basic input/output

---
+++ basics/swap_variable.py
@@ -0,0 +1,9 @@
+a = 5
+b = 10
+
+# Swapping the values of a and b
+temp = a # Store the value of a in a temporay variabele
+a = b  # Assign the value of b to a
+b = temp # Assign the value of temp (which is the original value of a )to b
+
+print("After swappping: a = ", a, "b = ", b)
