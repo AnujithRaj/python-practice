45f4f9d93bc033a646eea64726319e9726241cde
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-16 09:35:27 +0530

    Add List Program

---
+++ basics/list.py
@@ -0,0 +1,9 @@
+# This code demonstrates the creation and usage of a list in python.
+list1 = [1, 2, 3, 4, 5]
+
+# Printing the list, its type, and accessing individular elements using their index, where the index starts from 0.
+print(list1)
+print(type(list1))
+print(list1[0])  # Accessing the first element
+print(list1[1])  # Accessing the second element
+print(list1[4])
