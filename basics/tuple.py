9ad611bbb0046ea5ed17cb5c916f4b6a2d616d69
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-17 07:17:46 +0530

    creaat a tuple and Print its length

---
+++ basics/tuple.py
@@ -0,0 +1,4 @@
+tup1 = (2, 4, 6, 8)
+print(tup1)
+print(type(tup1)) # Printing the tuple and its type
+print(len(tup1)) # Accessing elements of the tuple using their index, where the index starts from 0.
