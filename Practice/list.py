e6fceb0273062bea7f68bd4e9f38c20cf28a9b78
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-20 14:42:56 +0530

    Adding List Practice

---
+++ Practice/list.py
@@ -0,0 +1,19 @@
+# Creating a list
+lis1 = [19, 31, 93, 53, 73]
+print(lis1)
+
+# Add a new element at the end
+lis1.append(41)
+print(lis1)
+
+# Remove an element.
+lis1.remove(31)
+print(lis1)
+
+# Find max and Min value
+print("Maximum Value: ", max(lis1))
+print("Minimum Value: ", min(lis1))
+
+# Reverse the List.
+lis1.reverse()
+print("Reverse List: ", lis1)
