622604562e4a0ea6756566dd3892e2c77da87f11
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-20 15:05:16 +0530

    Adding Indexing & Slicing In Python

---
+++ Practice/indexing&slicing.py
@@ -0,0 +1,16 @@
+# Creating a List
+lis2 = [95, 34, 67, 93, 53]
+# Printing First element of List
+print("Print First Element: ", lis2[0])
+
+# Printing Last Element of list
+print("Print Last Element: ", lis2[-1])
+
+# Slice First 3 Elements
+print("First 3 Elements: ", lis2[0:3])
+
+# Slice Last 2 Elements
+print("Last 2 Elemets: ", lis2[-2:])
+
+# Reverse a list Using Slicing
+print("Reverse List Using Slicing: ", lis2[::-1])
