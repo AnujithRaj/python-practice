450aecd8381bace74af0c54117b57307fb513b6b
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-21 14:42:46 +0530

    Create a Tuple and Find lenght

---
+++ Practice/tuple.py
@@ -0,0 +1,8 @@
+tup1 = (2, 4, 6, 8)
+print(tup1)
+
+# Printing the tuple and its type
+print(type(tup1))
+
+# Accessing elements of the tuple using their index, where the index starts from 0.
+print(len(tup1))
