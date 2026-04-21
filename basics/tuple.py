1507d348e33387a4c7b8333f8427f9609ec4c214
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-21 14:59:53 +0530

    Modified Tuple Code

--- basics/tuple.py
+++ basics/tuple.py
@@ -1,4 +1,18 @@
-tup1 = (2, 4, 6, 8)
-print(tup1)
-print(type(tup1)) # Printing the tuple and its type
-print(len(tup1)) # Accessing elements of the tuple using their index, where the index starts from 0.
+# Create a Tuple of 5 elements.
+tup = (95, 34, 67, 93, 53)
+print("Original Tuple: ", tup)
+
+# Tuple is immutable. You cannot change value, no add, no remove and no update.
+# tup[0] = 100   # ❌ Error (tuple is immutable)
+
+# Convert Tuple into List
+lst = list(tup)
+print("Converted to List: ", lst)
+
+# Modify Converted List
+lst.append(92)
+print("Modified List: ", lst)
+
+# Convert List into Tuple
+tup1 = tuple(lst)
+print("Converted Tuple: ", tup1)
