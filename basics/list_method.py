b9875518fae93c05baad2fd7f602b6d78a8527e1
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-21 14:38:23 +0530

    Add List Methods In Python

---
+++ basics/list_method.py
@@ -0,0 +1,19 @@
+# Creating a List
+lis1 = [62, 87, 57, 71, 88]
+print("Original List:", lis1)
+
+# Using append()
+lis1.append(95)
+print("After Append:", lis1)
+
+# Using insert()
+lis1.insert(1, 67)
+print("After Insert:", lis1)
+
+# Using Remove()
+lis1.remove(57)
+print("After Remove:", lis1)
+
+# Using pop()
+lis1.pop()
+print("After Pop:", lis1)
