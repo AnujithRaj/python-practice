9d02cf1e5b4ac95570406f4001044af5088cdc29
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-22 14:59:59 +0530

    String in Python

---
+++ basics/set.py
@@ -0,0 +1,2 @@
+# Creating a set with duplicate values
+set1 = [9, 5, 3, 4, 6, 7, 9, 3, 5, 3]
--- basics/string.py
+++ basics/string.py
@@ -1,4 +1,4 @@
-# This code demonstrates the creation and usage of a string in python.
+# String in Python
 Full_name = "Anujith Raj"
 # Printing the string, its type, and accessing individular characters using their index, where the index starts from 0.
 print(Full_name)
@@ -6,3 +6,36 @@ print(type(Full_name))
 print(Full_name[0])  # Accessing the first character
 print(Full_name[1])  # Accessing the second character
 print(Full_name[6])  # Accessing the seventh character
+
+# This Program Count the Number of Vowels in a string.
+str1 = "Siddharth"
+count = 0
+for i in str1:
+    if i in "aeiouAEIOU":
+        count += 1
+
+print("Vowels in String", count)
+
+# Reverse a String
+print("Original String: ", str1)
+
+revs = ""
+for i in str1:
+    revs = i + revs
+print("Reverse String: ", revs)
+
+# Check Palindrome.
+str2 = "Ladale"
+
+if str2.lower() == str2[::-1].lower():
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+# Convert string to Uppercase/Lowercase
+str3 = "upper"
+upper_str = str3.upper()
+print("Uppercase: ", upper_str)
+
+lower_str = str3.lower()
+print("Lowercase: ", lower_str)
