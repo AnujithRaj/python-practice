5145f6f5c5a29588416d4753e464d73bb6b43047
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-24 17:38:23 +0530

    Adding Arithmetic Operator in Pyhon

---
+++ basics/arithmetic_operator.py
@@ -0,0 +1,17 @@
+# Create 2 Number and perform all operations.
+a = 20
+b = 30
+print("Sum:", a + b)
+print("Subtract:", a - b)
+print("Multiply:", a * b)
+print("Divide:", a / b)
+print("Remainder:", a % b)
+print("Floor:", a // b)
+
+# Simple Interest
+p = 2000
+r = 8
+t = 2
+
+si = (p * r * t) / 100
+print("Simple Interest:", si)
