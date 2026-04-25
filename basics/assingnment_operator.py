d3d24ace22002e7abcc1eba0166ae2044ee71755
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-25 15:13:09 +0530

    Assignment Operators in Python

---
+++ basics/assingnment_operator.py
@@ -0,0 +1,16 @@
+# use +=, -=, *=, /=
+# Show how value change step by step.
+a = 11
+print("Initial:", a)
+
+a += 20
+print("After += 20:", a)
+
+a -= 5
+print("After -= 5:", a)
+
+a *= 2
+print("After *= 2:", a)
+
+a /= 2
+print("After /= 2:", a)
