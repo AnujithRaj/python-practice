25f2d081b499b73003e997aaaa22f551e0071148
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-27 14:59:18 +0530

    Add Identity Operator in Python

---
+++ basics/identity_operator.py
@@ -0,0 +1,15 @@
+# Compare two lists using is and ==
+lis1 = [2,3,4,5,6]
+
+# Case 1: Same reference
+lis2 = lis1
+
+print("Using 'is' (same object):", lis1 is lis2)
+print("Using '==' (same value):", lis1 == lis2)
+
+
+# Case 2: Different object, same value
+lis3 = [2,3,4,5,6]
+
+print("Using 'is' (different object):", lis1 is lis3)
+print("Using '==' (same value):", lis1 == lis3)
