4f7052aab3965281ecf07b816bab427ea16ffb0b
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-04-28 14:54:54 +0530

    Add For Loop in Python

---
+++ basics/for_loop.py
@@ -0,0 +1,12 @@
+# Print Number 1 to 10
+for i in range(1,11):
+    print(i)
+
+#Print Even Number.
+for i in range(2,21,2):
+    print("Even: ", i)
+
+# Find SUM of Numbers 1 to n.
+n = int(input("Enter Number:"))
+total = n * (n + 1) // 2
+print("Sum:", total)
