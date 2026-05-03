0c40bce16f86631bdf1957fe5cf7a1765fc5ce2e
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-03 08:48:01 +0530

    Adding Function Method in Python

--- basics/function.py
+++ basics/function.py
@@ -8,9 +8,49 @@ y = int(input("Enter second Number: "))
 print("sum: ",add(x,y))
 
 # Function to check Even/Odd
-def num():
+def num(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+num(5)
     
 
-# function to find Factorial,
+# function to find Factorial.
+def factorial(n):
+    if n < 0:
+        return "Invalid Number"
+    
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+
+    return result
+
+num = int(input("Enter Number: "))
+print("Factorial: ", factorial(num))
+    
 # Function with default arguments.
+def greet(name="Guest"):
+    print("Hello", name)
+
+greet("Siddharth")
+greet()
+
+# Add
+def add(a, b = 5):
+    print("Sum: ", a + b)
+
+add(2,8)
+add(7)
+
+
 # Function returning multiple values.
+def num(a,b):
+    sum = a + b
+    diff = a - b
+    return sum, diff
+
+result = num(30,20)
+print(result)
