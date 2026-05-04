e96890bf4816eafdb790a0dbd5a3a200c41e6ea0
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-04 14:58:39 +0530

    Add Simple Calculator Using Function

---
+++ Practice/calculator.py
@@ -0,0 +1,44 @@
+# Create simple calculator using functions.
+
+# Function for Operations
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+# Input
+a = int(input("Enter first Number: "))
+b = int(input("Enter second Number: "))
+
+print("Choose operation: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+# Logic
+if choice == "1":
+    print("Result: ", add(a, b))
+elif choice == "2":
+    print("Result: ", subtract(a, b))
+elif choice == "3":
+    print("Result: ", multiply(a, b))
+elif choice == "4":
+    print("Result: ", divide(a, b))
+else:
+    print("Invalid choice")
+
+
+
+ 
